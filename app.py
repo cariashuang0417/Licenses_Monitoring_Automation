29b8d0a106f4109071e72cded3aba2fe8afa574e
Shuang Song <103439749+cariashuang0417@users.noreply.github.com>
2023-02-26 01:13:41 -0500

    change format and add comments for better presenting

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import smtplib
 # Create the datetime representing current time
 now = dt.datetime.now()
 
+
 def expires_in_30_days_or_expired(datetime_obj):
     """Return a boolean value to represent whether a datetime is within 30 
     days from now."""
@@ -21,6 +22,7 @@ def expires_in_30_days_or_expired(datetime_obj):
         else:
             return False
 
+
 def datetime_of_timestamp(timestamp_value) -> dt.datetime:
     """Esri stores dates as timestamps in milliseconds. Use this 
     function to return a datetime object from a timestamp value."""
@@ -28,25 +30,46 @@ def datetime_of_timestamp(timestamp_value) -> dt.datetime:
     dt_value = dt.datetime.fromtimestamp(timestamp_epoch)
     return dt_value
 
+
 def get_server_licenses(gis):
+    """
+    returns a dictionary with all licenses from hosting server
+    """
+
+    # gis.admin.servers - Server Manager object
+    # gis.admin.servers.get() - Server object
+    # get hosting server objects from ArcGIS Enterprise using gis.admin.servers submodule
     hosting_servers = gis.admin.servers.get(role="HOSTING_SERVER")
+
+    # if no hosting server, return empty dictionary
     if not hosting_servers:
-        # no hosting server configured for gis instance
         return {}
+
+    # hosting_server.system - System Manager object
+    # return licenses from hosting server
     return hosting_servers[0].system.licenses
+
 
 def get_license_status(gis):
     """
-    Returns a dict of feature name keys
-    with values of True if license is expiring/expired,
-    or False if the license is valid through the threshold
+    Returns a dict of feature name keys with values of
+    True if license is expiring/expired, or False if the license is valid through the threshold
     """
+
     server_license_status = {}
-    for feature_license in get_server_licenses(gis).get('features', []):
+
+    # get license feature objects, which contains expiration info
+    feature_licenses = get_server_licenses(gis).get('features', [])
+
+    # loop through each license and get expiration date
+    # validate if license is expiring or not
+    for feature_license in feature_licenses:
         date_of_expiration = datetime_of_timestamp(feature_license['expiration'])
         is_expiring_concern = expires_in_30_days_or_expired(date_of_expiration)
         server_license_status[feature_license['displayName']] = is_expiring_concern
+
     return server_license_status
+
 
 def send_report_email(license_status):
     license_summary = "\r\n".join([f"{i[0]}: {'Requires Attention' if i[1] else 'Valid'}" for i in license_status.items()])
@@ -57,10 +80,13 @@ ArcGIS Licenses Report:
     """
     send_smtp(["jroebuck@esri.com", "ssong@esri.com"], message)
 
-def send_smtp(recipients, message,
-                    subject="ArcGIS Licensing Report"):
-    """Sends the `message` string to all of the emails in the 
-    `recipients` using the configured SMTP email server."""
+
+def send_smtp(recipients, message, subject="ArcGIS Licensing Report"):
+    """
+    Sends the `message` string to all of the emails in the
+    `recipients` using the configured SMTP email server.
+    """
+
     smtp_config = get_smtp_config()
     smtp_server_url = smtp_config.get('server')
     sender= smtp_config.get('username')
@@ -91,12 +117,14 @@ def send_smtp(recipients, message,
     server.quit()
     return did_succeed
 
+
 def get_smtp_config():
     return {
         "server": os.environ.get("SMTP_SERVER"),
         "username": os.environ.get("SMTP_USERNAME"),
         "password": os.environ.get("SMTP_PASSWORD"),
     }
+
 
 def get_main_args():
     args = sys.argv[-3:] # last 3 args
@@ -109,6 +137,7 @@ def get_main_args():
         print('Please pass url, username, password as ordered args')
         sys.exit(1)
     return args
+
 
 def main():
     url, username, password = get_main_args()
@@ -125,6 +154,7 @@ def main():
         sys.exit(1)
     else:
         print('All Licenses Ok!')
+
 
 if __name__ == '__main__':
     main()
